7bf2a74c1e4b8cf9dc5219d94fd6222e18788722
ivanb79 <ivanbesker99@gmail.com>
2025-10-13 10:16:30 +0200

    Add files via upload
    Ivan Borna Bešker vjezba1

---
+++ vj1ibb.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//using namespace std;
+//
+//int ogranicenje(int vrijednost, int low = 0, int high = 100) {
+//	if (vrijednost < low)
+//		return low;
+//	else if (vrijednost > high)
+//		return high;
+//	else
+//		return vrijednost;
+//}
+//double ogranicenje(double vrijednost, double low = 0.0, double high = 100.0) {
+//	if (vrijednost < low)
+//		return low;
+//	else if (vrijednost > high)
+//		return high;
+//	else
+//		return vrijednost;
+//}
+//int main() {
+//	cout << "INT testovi:\n";
+//	cout << "ogranicenje(-5) = " << ogranicenje(-5) << std::endl;
+//	cout << "ogranicenje(50) = " << ogranicenje(50) << std::endl;
+//	cout << "ogranicenje(150) = " << ogranicenje(150) << std::endl;
+//
+//	cout << "\nDOUBLE testovi:\n";
+//	cout << "ogranicenje(-2.5) = " << ogranicenje(-2.5) << std::endl;
+//	cout << "ogranicenje(42.7) = " << ogranicenje(42.7) << std::endl;
+//	cout << "ogranicenje(123.45) = " << ogranicenje(123.45) << std::endl;
+//	return 0;//}
